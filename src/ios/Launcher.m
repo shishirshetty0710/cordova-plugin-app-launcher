0493b190bcee5d5cf89042e5694fe08e68090247
Shishir Shetty <shishirshetty0710@gmail.com>
2024-04-09 16:04:39 -0400

    Update Launcher.m

--- src/ios/Launcher.m
+++ src/ios/Launcher.m
@@ -23,7 +23,6 @@
 }
 
 - (void)launch:(CDVInvokedUrlCommand*)command {
-	NSDictionary* options = [command.arguments objectAtIndex:0];
 	CDVPluginResult * pluginResult = nil;
 
 	NSURL *launchURL = [NSURL URLWithString:@"epicmychart://orgselect?orgID=1353"];
@@ -32,24 +31,8 @@
 		[[UIApplication sharedApplication] openURL:launchURL];
 
 	} else {
-		NSURL *launchURL = [NSURL URLWithString:@"https://itunes.apple.com/us/app/mychart/id382952264?mt=8"];
-		[[UIApplication sharedApplication] openURL:launchURL];
-	}
-
-	if ([options objectForKey:@"uri"]) {
-		NSString *uri = [options objectForKey:@"uri"];
-		if ([[UIApplication sharedApplication] canOpenURL: [NSURL URLWithString:uri]]) {
-			NSURL *launchURL = [NSURL URLWithString:uri];
-			[[UIApplication sharedApplication] openURL: launchURL];
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-		} else {
-			pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"No app installed that can handle that uri."];
-			[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-		}
-	} else {
-		pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Missing option: 'uri' required."];
-		[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+		NSURL *launchURL_ = [NSURL URLWithString:@"https://itunes.apple.com/us/app/mychart/id382952264?mt=8"];
+		[[UIApplication sharedApplication] openURL:launchURL_];
 	}
 }
 
